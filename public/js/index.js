ac7039de16282d8e33e4fdc2b6d9969d81f0591d
7042094357 <r119d23@gmail.com>
2021-01-12 21:39:55 +0530

    Worked on broadcast messaging

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,6 @@
 var socket = io();
 socket.on('connect',function(){
   console.log('connected to server');
-
-  socket.emit('createMessage',{
-    from: 'Rishabh Tyagi',
-    text: 'I am good wbu',
-    createdAt: 321
-  });
 });
 
 socket.on('disconnect',function(){
