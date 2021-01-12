eb5d469f295f66d4fb0a710648642ab2bf665dac
7042094357 <r119d23@gmail.com>
2021-01-12 22:07:00 +0530

    Message generator defined and tested

---
+++ server/utils/message.test.js
@@ -0,0 +1,16 @@
+var expect = require('expect')
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage',()=>{
+  it('should generate the correct message object',()=>{
+    var from = 'Rishabh';
+    var text = 'Some message to test';
+
+    var message = generateMessage(from,text);
+
+    expect(message.from).toBe(from);
+    expect(message.text).toBe(text);
+    expect(typeof message.createdAt).toBe('number');
+  });
+});
