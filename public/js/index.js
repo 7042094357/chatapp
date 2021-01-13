a2106270872899a65d3bac0dc49e4e1783b68327
7042094357 <r119d23@gmail.com>
2021-01-13 10:53:17 +0530

    Form for sending message added

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+// var {generateMessage} = require('./../server/utils/message');
+
 var socket = io();
 socket.on('connect',function(){
   console.log('connected to server');
@@ -6,6 +8,31 @@ socket.on('connect',function(){
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
 });
+
 socket.on('newMessage', function(message){
   console.log('New Message', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from} : ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage',generateMessage('Shikha','How is this going?'));
+
+// socket.emit('createMessage',{
+//   from : 'Shikha',
+//   text : 'How is this going?'
+// },function(msgs){
+//   console.log('Got it',msgs);
+// });
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage',{
+    from : 'User',
+    text : jQuery('[name=message]').val()
+  },function(){
+
+  })
 });
