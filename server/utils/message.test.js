ccdcbe6f9c4ccdf4bd303305a61f47918ddd616c
7042094357 <r119d23@gmail.com>
2021-01-13 12:22:40 +0530

    Added one test case and geolocation terminology

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect')
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',()=>{
   it('should generate the correct message object',()=>{
@@ -11,6 +11,20 @@ describe('generateMessage',()=>{
 
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
+    expect(typeof message.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage',()=>{
+  it('should generate the correct location object',()=>{
+    var from = 'Rishabh';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'http://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from,latitude,longitude);
+
+    expect(message.from).toBe(from);
+    expect(message.url).toBe(url);
     expect(typeof message.createdAt).toBe('number');
   });
 });
