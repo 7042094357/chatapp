60ba3af654f89d311c3982037a0b49b3be27c531
7042094357 <r119d23@gmail.com>
2021-01-13 12:49:32 +0530

    User Interface added

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) =>{
     console.log('Message received', message);
 
     io.emit('newMessage',generateMessage(message.from , message.text));
-    callback('This is from server.');
+    callback();
     // socket.broadcast.emit('newMessage',{
     //   from : message.from,
     //   text : message.text,
