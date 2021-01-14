baf268aa3c9513241d414246f0ef6a460f6d8f3a
7042094357 <r119d23@gmail.com>
2021-01-14 11:55:41 +0530

    Almost done

--- server/server.js
+++ server/server.js
@@ -7,11 +7,13 @@ const {generateMessage,generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'/../public');
 const port = process.env.PORT || 3000;
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -20,9 +22,13 @@ io.on('connection', (socket) =>{
 
   socket.on('join', (params,callback)=>{
     if(!isRealString(params.name) || !isRealString(params.room)){
-      callback('Name and room are required to join');
+      return callback('Name and room are required to join');
     }
     socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id,params.name,params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
     socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
     socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined`));
     callback();
@@ -44,7 +50,13 @@ io.on('connection', (socket) =>{
     io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
   });
   socket.on('disconnect', ()=>{
-    console.log('Client disconnected');
+    var user = users.removeUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin',`${user.name} has left the room`));
+    }
+    // console.log('Client disconnected');
   });
 });
 
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -5,6 +5,24 @@ const {Users} = require('./users');
 
 
 describe('Users',()=>{
+  var users;
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id : '1',
+      name : 'Rishabh',
+      room : 'The friends'
+    },{
+      id : '2',
+      name : 'Shikha',
+      room : 'Hollow'
+    },{
+      id : '3',
+      name : 'Favorite',
+      room : 'The friends'
+    }];
+  });
+
   it('Should add users',()=>{
     var users = new Users();
     var user = {
@@ -15,5 +33,48 @@ describe('Users',()=>{
     var resUser = users.addUser(user.id,user.name,user.room);
 
     expect(users.users).toEqual([user]);
+  });
+
+  it('Should remove a user',()=>{
+    var userId = '2';
+    var user = users.removeUser(userId);
+
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('Should not remove a user',()=>{
+    var userId = '7';
+    var user = users.removeUser(userId);
+
+    expect(user).toBeFalsy();
+    expect(users.users.length).toBe(3);
+  });
+
+  it('Should find a user',()=>{
+    var userId = '2';
+    var user = users.getUser(userId);
+
+    expect(user.id).toBe(userId);
+  });
+
+
+  it('Should not find a user',()=>{
+    var userId = '5';
+    var user = users.getUser(userId);
+
+    expect(user).toBeFalsy();
+  });
+
+
+  it('Should return name for The friends',()=>{
+    var userList = users.getUserList('The friends');
+    expect(userList).toEqual(['Rishabh','Favorite']);
+  });
+
+
+  it('Should return name for Hollow',()=>{
+    var userList = users.getUserList('Hollow');
+    expect(userList).toEqual(['Shikha']);
   });
 });
